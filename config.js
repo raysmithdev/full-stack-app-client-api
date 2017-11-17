19d1b1c027abcb9bcee02be25e0ad58149d277b6
Raymond Smith <raysmithdev@gmail.com>
2017-11-16 16:03:11 -0800

    #

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ exports.DATABASE_URL =
     process.env.DATABASE_URL ||
     global.DATABASE_URL ||
     'mongodb://localhost/my-app';
-// exports.JWT_SECRET = process.env.JWT_SECRET;
-// exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
+exports.JWT_SECRET = process.env.JWT_SECRET || 'ffgffgfgf';
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '7d';
 exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000/';
+exports.PORT = process.env.PORT || 8080;
--- server.js
+++ server.js
@@ -1,15 +1,41 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const passport = require('passport');
+
 const {CLIENT_ORIGIN} = require('./config');
-const PORT = process.env.PORT || 3000;
+
+const {router: usersRouter} = require('./user/router');
+const {basicStrategy, jwtStrategy} = require('./strategies');
+
+mongoose.Promise = global.Promise;
+
+const {PORT, DATABASE_URL} = require('./config');
+
+app.use(passport.initialize());
+passport.use(basicStrategy);
+passport.use(jwtStrategy);
+
+app.use('/api/users/', usersRouter);
+
+// A protected endpoint which needs a valid JWT to access it
+app.get(
+    '/api/protected',
+    passport.authenticate('jwt', {session: false}),
+    (req, res) => {
+        return res.json({
+            data: 'rosebud'
+        });
+    }
+);
 
 app.use(
     cors({
         origin: CLIENT_ORIGIN
     })
 );
-
 app.get('/api/*', (req, res) => {
   res.json({ok: true});
 });
